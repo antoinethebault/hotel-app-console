b13f894889807db2e8ab7e40624887ec7f80c864
antoinethebault <antoine.thebau@gmail.com>
2020-04-21 16:21:45 +0200

    js avances 3

--- service.js
+++ service.js
@@ -1,9 +1,10 @@
 
 var request = require('request');
 var requestPromise = require('request-promise-native')
+const url = 'http://localhost:8080/clients';
 
 function listerClients(clientsFonction){
-    requestPromise('http://localhost:8080/clients', 
+    requestPromise(url, 
     { json: true })
     .then((body) => clientsFonction(null, null, body))
     .catch((err) => clientsFonction(err, null, null))
@@ -18,19 +19,14 @@ function creerClient(creerClientFonction){
 exports.creerClient = creerClient;
 
 function ajouterClient(nom, prenom, callbackErr, callbackSuccess){
-    request('http://localhost:8080/clients', { json: true,
+    requestPromise(url, { json: true,
         method: 'POST',
         body: {
             nom : nom,
             prenoms : prenom
-        }
-    }, (err, res, body) => {
-        if (err) { 
-            callbackErr(err)
-        } else {
-            callbackSuccess(body)
-        }
-    });
+        }})
+        .then((body) => callbackSuccess(body))
+        .catch((err) => callbackErr(err));
 }
 
 exports.ajouterClient = ajouterClient;
