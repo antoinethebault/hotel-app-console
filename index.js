e6a594979aa35f55ca4865824c8c683b624ac81f
antoinethebault <antoine.thebau@gmail.com>
2020-04-22 16:43:12 +0200

    duplicate files js

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//var presentation = require('./presentation.js');
-var presentation_1 = require("./presentation");
-presentation_1.choisir();
